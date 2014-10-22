e1c95992b6dbef3c76cc22c24aa3081494903661
cs1384 <ytl264@nyu.edu>
2014-10-22 15:53:42 -0400

    hao2

--- src/edu/nyu/cs/cs2580/IndexerInvertedDoconly.java
+++ src/edu/nyu/cs/cs2580/IndexerInvertedDoconly.java
@@ -47,6 +47,9 @@ public class IndexerInvertedDoconly extends Indexer implements Serializable {
   private Map<String, Vector<Integer>> _index =
       new HashMap<String, Vector<Integer>>();
   
+  private Map<String, int []> _tindex =
+	      new HashMap<String, int []>();
+  
   private class Posting implements Serializable{
     public int did;
     //get occurance by offsets.size()
@@ -87,25 +90,6 @@ public class IndexerInvertedDoconly extends Indexer implements Serializable {
 		        }
 
 		    };
-
-  public void mergeAllFiles(int count){
-	  System.out.println("Merging files...");
-      try
-      {
-          File f = new File(_options._indexPrefix + "/partial_cmpr_corpus_merged_0.txt");
-          f.createNewFile();            
-
-          for(int i=0;i<count;i++)
-          {
-              mergeIndices(i) ;
-              System.out.println("Merged " + (i+1) + " / " + count);
-          }
-      }
-      catch(Exception e)
-      {
-          e.printStackTrace();;
-      } 
-  }
   
   public  void mergeIndices(int id) 
   {
@@ -189,19 +173,31 @@ public class IndexerInvertedDoconly extends Indexer implements Serializable {
   }
   
   
+  public void mergeAllFiles(int count){
+	  System.out.println("Merging files...");
+      try
+      {
+          File f = new File(_options._indexPrefix + "/partial_cmpr_corpus_merged_0.txt");
+          f.createNewFile();            
+
+          for(int i=0;i<count;i++)
+          {
+              mergeIndices(i) ;
+              System.out.println("Merged " + (i+1) + " / " + count);
+          }
+      }
+      catch(Exception e)
+      {
+          e.printStackTrace();;
+      } 
+  }
+  
+  
   
   @Override
   public void constructIndex() throws IOException
   {
     printRuntimeInfo("======= GO WSE_homework2!! =======");
-    /*
-    File f1 = new File(_options._indexPrefix + "/index0new.txt");
-    File f2 = new File(_options._indexPrefix + "/index1new.txt");
-    File f3 = new File(_options._indexPrefix + "/index2new.txt");
-    f1.createNewFile();
-    f2.createNewFile();
-    f3.createNewFile();
-    */
     try
     {
       String corpusFolder = _options._corpusPrefix + "/";
@@ -229,35 +225,7 @@ public class IndexerInvertedDoconly extends Indexer implements Serializable {
       }
       _roundtake = takeTurn;
       mergeAllFiles(takeTurn);
-      //finalConstruction(takeTurn);
-      /*
-      BufferedReader br = new BufferedReader(new FileReader(new File(_options._indexPrefix + "/partial_cmpr_corpus_merged_" + takeTurn + ".txt")));
-      String line = br.readLine();
-      
-      _op.clear();
-      
-      int i, op;
-      while(line!=null){
-    	Vector<Posting> freq = new Vector<Posting>();
-        String[] list = line.split(" ");
-        //System.out.println(list[0]);
-        for(i=1;i<list.length;i++){
-          //System.out.println(list[i]);
-          Posting newp = new Posting(Integer.parseInt(list[i]));
-          freq.add(newp);
-        }
-        _op.put(list[0],freq);
-        line = br.readLine();
-        
-        File f2 = new File(_options._indexPrefix + "/partial_cmpr_corpus_merged_" + takeTurn + ".txt");
-        f2.delete();
-      
-      }
-      br.close();
-*/
-      
-      
-    }catch (Exception e){
+          }catch (Exception e){
         e.printStackTrace();
     }
     
@@ -310,10 +278,6 @@ private void constructPartialIndex(List<File> listOfFiles){
         for(Posting p : pv)
         {
           sb.append(" ").append(p.did);
-          //sb.append(" ").append(p.offsets.size());
-          //for(Integer offset : p.offsets){
-          //  sb.append(" ").append(offset);
-          //}
         }
         //System.out.println(sb.toString());
         sb.append("\n");
@@ -337,22 +301,43 @@ private void constructPartialIndex(List<File> listOfFiles){
   public void makeIndex(){
     try{
     	
-        
+      
       BufferedReader br = new BufferedReader(new FileReader(new File(_options._indexPrefix + "/partial_cmpr_corpus_merged_"+ _roundtake +".txt")));
       String line = br.readLine();
+      //Vector<Integer> freq = new Vector<Integer>();
       int i, op;
+      String[] list;
+      String term;
+      printRuntimeInfo("========Loading indices=======");
       while(line!=null){
     	//System.out.println(line);
-        Vector<Integer> freq = new Vector<Integer>();
-        String[] list = line.split(" ");
+    	//Vector<Integer> freq = new Vector<Integer>();
+        //freq.clear();
+        //Scanner st = new Scanner(line);
+        //String term = st.next();
+        list = line.split(" ");
+    	/*
+        while (st.hasNext()){
+        	this._totalTermFrequency++;
+        	freq.add(Integer.parseInt(st.next()));
+        }
+        freq.trimToSize();
+        _index.put(term,freq);
+        line = br.readLine();
+        st.close();
+        */
+        term = list[0];
+        int [] freq = new int [list.length - 1];
+        this._totalTermFrequency += (list.length-1);
         for(i=1;i<list.length;i++){
           op = Integer.parseInt(list[i]);
-          freq.add(op);
+          freq[i-1] = op;
         }
-        _index.put(list[0],freq);
         line = br.readLine();
+        _tindex.put(term, freq);
       }
       br.close();
+      printRuntimeInfo("======== END!! ========");
     }catch (IOException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
@@ -405,40 +390,10 @@ private void constructPartialIndex(List<File> listOfFiles){
     	_totalTermFrequency++;
     }
     
-    /*
-    String token;
-    while (s.hasNext()) {
-      //put offsets into op map
-      token = s.next();
-      if(_op.containsKey(token)){
-        if(_op.get(token).lastElement().did==docid){
-          _op.get(token).lastElement().offsets.add(offset);
-        }else{
-          Vector<Integer> offsetTracker = new Vector<Integer>();
-          offsetTracker.add(offset);
-          Posting posting = new Posting(docid);
-          posting.offsets = offsetTracker;
-          _op.get(token).add(posting);
-        }
-      }else{
-        Vector<Integer> offsetTracker = new Vector<Integer>();
-        offsetTracker.add(offset);
-        Posting posting = new Posting(docid);
-        posting.offsets = offsetTracker;
-        Vector<Posting> list = new Vector<Posting>();
-        list.add(posting);
-        _op.put(token, list);
-      }
-      offset++;
-      _totalTermFrequency++;
-    }
-    s.close();
-    */
   }
 
   @Override
   public void loadIndex() throws IOException, ClassNotFoundException {
-	printRuntimeInfo("========Loading indices=======");
     String indexFile = _options._indexPrefix + "/DoconlyIndexer.idx";
     System.out.println("Load index from: " + indexFile);
 
@@ -453,20 +408,13 @@ private void constructPartialIndex(List<File> listOfFiles){
     makeIndex();
     // Compute numDocs and totalTermFrequency b/c Indexer is not serializable.
     this._numDocs = _documents.size();
-    this._totalTermFrequency = 0;
-    int length = 0;
-    for (String rc : _index.keySet()) {
-    	length = _index.get(rc).size();
-    	//System.out.println(length);
-    	this._totalTermFrequency += length;
-    }
    
     this._urlToDoc = loaded._urlToDoc;
     reader.close();
     
     System.out.println(Integer.toString(_numDocs) + " documents loaded " +
         "with " + Long.toString(_totalTermFrequency) + " terms!");
-    printRuntimeInfo("======== END!! ========");
+    
   }
 
   @Override
@@ -480,7 +428,7 @@ private void constructPartialIndex(List<File> listOfFiles){
    */
 
   @Override
-  public Document nextDoc(Query query, int docid) {
+  public Document nextDoc(QueryPhrase query, int docid) {
       Vector<String> words = query._tokens;
       int length = words.size();
       Vector<Integer> newidlist = new Vector<Integer>();
@@ -507,12 +455,20 @@ private void constructPartialIndex(List<File> listOfFiles){
   
   @Override
   public int corpusDocFrequencyByTerm(String term) {
-    return 0;
+      int freq = 0;
+	  if (_index.containsKey(term)){
+    	  freq = _index.get(term).size();
+      }
+	  return freq;
   }
 
   @Override
   public int corpusTermFrequency(String term) {
-    return 0;
+	  int freq = 0;
+	  if (_index.containsKey(term)){
+    	  freq = _index.get(term).size();
+      }
+	  return freq;
   }
 
   @Override
@@ -526,267 +482,32 @@ private void constructPartialIndex(List<File> listOfFiles){
 	int postingLength = postingList.size();
 	int minID = postingList.firstElement();
 	int maxID = postingList.lastElement();
-	if (maxID == 0 || current > maxID){
+	if (maxID == 0 || current >= maxID){
 		return 99999999;
 	}else if (minID > current){
 		return minID;
 	}else{
 		int i = 0;
-		while(postingList.get(i) < current){
+		while(postingList.get(i) <= current){
 			i++;
 		}
 		return postingList.get(i);
 	}
-	
-	
-  }
-
-  /*
-  public Document nextDoc(QueryPhrase query, int docid) {
-    boolean keep = false;
-    int did = docid;
-    //keep getting document until no next available 
-    while((did = nextDocByTerms(query._tokens,did))!=Integer.MAX_VALUE){
-      keep = false;
-      //check if the resulting doc contains all phrases 
-      for(Vector<String> phrase : query._phrases){
-        //if not, break the for loop and get next doc base on tokens
-        if(nextPositionByPhrase(phrase,did,-1)==Integer.MAX_VALUE){
-          keep = true;
-          break;
-        }
-      }
-      if(keep){
-        continue;
-      }else{
-        //create return object if passed all phrase test and return
-        DocumentIndexed result = new DocumentIndexed(did);
-        return result;
-      }
-    }
-    //no possible doc available
-    return null;
-  }
-  /*
-  public int nextPositionByPhrase(Vector<String> phrase, int docid, int pos){
-    int did = nextDocByTerms(phrase, docid-1);
-    //System.out.println("docid"+docid);
-    if(docid != did){
-      return Integer.MAX_VALUE;
-    }
-    int position = nextPositionByTerm(phrase.get(0), docid, pos);
-    boolean returnable = true;
-    int largestPos = position;
-    int i = 1;
-    int tempPos;
-    for(;i<phrase.size();i++){
-      tempPos = nextPositionByTerm(phrase.get(i), docid, pos);
-      //one of the term will never find next
-      if(tempPos==Integer.MAX_VALUE){
-        return Integer.MAX_VALUE;
-      }
-      if(tempPos>largestPos){
-        largestPos = tempPos;
-      } 
-      if(tempPos!=position+1){
-        returnable = false;
-      }else{
-        position = tempPos;
-      }
-    }    
-    if(returnable){
-      return position;
-    }else{
-      return nextPositionByPhrase(phrase, docid, largestPos);
-    }
-    
-  }
- 
-  public int nextPositionByTerm(String term, int docid, int pos){
-    Vector<Posting> list = this.getPostingList(term);
-    //System.out.println("size"+list.size());
-    if(list.size()>0){
-      Posting op = binarySearchPosting(list, 0, list.size()-1, docid);
-      
-      if(op==null){
-        return Integer.MAX_VALUE; 
-      }
-      int largest = op.offsets.lastElement();
-      if(largest <= pos){
-        return Integer.MAX_VALUE;
-      }
-      if(op.offsets.firstElement() > pos){
-        return op.offsets.firstElement();
-      }
-      return binarySearchOffset(op.offsets,0,op.offsets.size(),pos);
-    }
-    return Integer.MAX_VALUE;
-  }
-
-  public int binarySearchOffset(Vector<Integer> offsets, int low, int high, int pos){
-    int mid;
-    while((high-low)>1){
-      mid = (low+high)/2;
-      if(offsets.get(mid) <= pos){
-        low = mid;
-      }else{
-        high = mid;
-      }
-    }
-    return offsets.get(high);
   }
   
-  public Posting binarySearchPosting(
-      Vector<Posting> list, int low, int high, int docid){
-    int mid;
-    while((high-low)>1){
-      mid = (low+high)/2;
-      if(list.get(mid).did < docid){
-        low = mid;
-      }else{
-        high = mid;
-      }
-    }
-    if(list.get(high).did==docid){
-      return list.get(high);
-    }else{
-      return list.get(low);
-    }
-  }
-  
-  public int nextDocByTerms(Vector<String> terms, int curDid){
-    if(terms.size()<=0){
-      if(curDid<=0){
-        return 1;
-      }else if(curDid>=_numDocs){
-        return Integer.MAX_VALUE;
-      }else{
-        return curDid+1;
-      }
-    }
-    //System.out.println("term" + curDid);
-    int did = nextDocByTerm(terms.get(0), curDid);
-    boolean returnable = true;
-    int largestDid = did;
-    int i = 1;
-    int tempDid;
-    for(;i<terms.size();i++){
-      tempDid = nextDocByTerm(terms.get(i), curDid);
-      //one of the term will never find next
-      if(tempDid==Integer.MAX_VALUE){
-        return Integer.MAX_VALUE;
-      }
-      if(tempDid>largestDid){
-        largestDid = tempDid;
-      } 
-      if(tempDid!=did){
-        returnable = false;
-      }
-    }    
-    if(returnable){
-      return did;
-    }else{
-      return nextDocByTerms(terms, largestDid-1);
-    }
-  }
-  
-  public int nextDocByTerm(String term, int curDid){
-    Vector<Posting> op = this.getPostingList(term);
-    if(op.size()>0){
-      int largest = op.lastElement().did;
-      //System.out.println("largest"+largest);
-      if(largest <= curDid){
-        return Integer.MAX_VALUE;
-      }
-      if(op.firstElement().did > curDid){
-        return op.firstElement().did;
-      }
-      return binarySearchDoc(op,0,op.size()-1,curDid);
-    }else
-      return Integer.MAX_VALUE;
-  }
-  
-  public int binarySearchDoc(Vector<Posting> op, int low, int high, int curDid){
-    int mid;
-    while((high-low)>1){
-      mid = (low+high)/2;
-      if(op.get(mid).did <= curDid){
-        low = mid;
-      }else{
-        high = mid;
-      }
-    }
-    return op.get(high).did;
+  private void Docall(QueryPhrase query){
+	  int i = -1;
+	  System.out.println("All the files that contain words: " + query._tokens);
+	  DocumentIndexed result = (DocumentIndexed) nextDoc(query, i);
+	  System.out.println(result._docid);
+	  
+	  while(result != null){
+		  System.out.println(result._docid);
+		  result = (DocumentIndexed) nextDoc(query, result._docid+1);
+	  }
+	  
   }
 
-  @Override
-  public int corpusDocFrequencyByTerm(String term) {
-    Vector<Posting> temp = getPostingList(term);
-    return temp.size();
-  }
-  
-  public Vector<Posting> getPostingList(String term){
-    Vector<Posting> result = new Vector<Posting>(); 
-    if(!_index.containsKey(term)){
-      return result;
-    }else{
-      int lineN = _index.get(term).lineN;
-      //System.out.println("lineN:" + lineN);
-      try {
-        File file = new File(_options._indexPrefix + "/Occurance_index.txt");
-        String line = FileUtils.readLines(file).get(lineN-1).toString();
-        //System.out.println("line:" + line);
-        String[] tokens = line.split(" ");
-        int i,top,offsetN;
-        for(i = 1;i<tokens.length;){
-          Posting posting = new Posting(Integer.parseInt(tokens[i++]));
-          offsetN = Integer.parseInt(tokens[i++]);
-          posting.offsets = new Vector<Integer>(offsetN);
-          top = i+offsetN;
-          while(i<top){
-            posting.offsets.add(Integer.parseInt(tokens[i++]));
-          }
-          result.add(posting);
-        }
-      } catch (FileNotFoundException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
-      return result;
-    }
-  }
-
-  @Override
-  public int corpusTermFrequency(String term) {
-    int result = 0;
-    Vector<Posting> op = getPostingList(term);
-    for(Posting ps : op){
-      result += ps.offsets.size();
-    }
-    return result;
-  }
-
-  @Override
-  public int documentTermFrequency(String term, String url) {
-    
-    String[] temp = url.split("/");
-    String key = temp[temp.length-1];
-    if(_urlToDoc.containsKey(key)){
-      int did = _urlToDoc.get(key);
-      QueryPhrase query = new QueryPhrase(term);
-      DocumentIndexed di = (DocumentIndexed)nextDoc(query,did-1);
-      if(di!=null){
-        return di.getOccurance();
-      }else{
-        return 0;
-      }
-    }
-    return 0;
-  }
-*/  
   public void printRuntimeInfo(String msg){
     System.out.println();
     System.out.println(msg);
@@ -800,36 +521,25 @@ private void constructPartialIndex(List<File> listOfFiles){
   }
   
   public static void main(String args[]){
-    /*
-    File file = new File("/Users/Tin/Desktop/test.txt");
-    try {
-      String line = FileUtils.readLines(file).get(2).toString();
-      System.out.println("|" + line + "|");
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    */
-    
-    
     try {
       Options options = new Options("conf/engine.conf");
       IndexerInvertedDoconly a = new IndexerInvertedDoconly(options);
       //a.constructIndex();
       a.loadIndex();
-      //mergeAllFiles(1);
-      
+      /*
       //QueryPhrase q11 = new QueryPhrase("new york");
       QueryPhrase q11 = new QueryPhrase("new york city");
       QueryPhrase q12 = new QueryPhrase("new york film");
       QueryPhrase q13 = new QueryPhrase("bertagna");
-      DocumentIndexed d11 = (DocumentIndexed) a.nextDoc(q11, 100);
+      DocumentIndexed d11 = (DocumentIndexed) a.nextDoc(q11, 105);
       System.out.println(d11._docid);
-      DocumentIndexed d12 = (DocumentIndexed) a.nextDoc(q12, 100);
+      DocumentIndexed d12 = (DocumentIndexed) a.nextDoc(q12, 126);
       System.out.println(d12._docid);
-      DocumentIndexed d13 = (DocumentIndexed) a.nextDoc(q13, 100);
+      DocumentIndexed d13 = (DocumentIndexed) a.nextDoc(q13, 1000);
       System.out.println(d13._docid);
-      
+      //a.Docall(q11);
+      //a.Docall(q12);
+      //a.Docall(q13);
       /*
       QueryPhrase q11 = new QueryPhrase("kicktin");
       DocumentIndexed d11 = (DocumentIndexed) a.nextDoc(q11, -1);
@@ -845,6 +555,5 @@ private void constructPartialIndex(List<File> listOfFiles){
       e.printStackTrace();
     }
     
-  }
-  
+  }  
 }
